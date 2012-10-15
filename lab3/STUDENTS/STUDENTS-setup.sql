ca6155b33b71fdc166fc0d717465d825de72e356
Kevin Shibata <kkshibat@unix2.csc.calpoly.edu>
2012-10-15 02:14:18 -0700

    WTF

--- lab3/STUDENTS/STUDENTS-setup.sql
+++ lab3/STUDENTS/STUDENTS-setup.sql
@@ -2,16 +2,16 @@ rem Kevin Shibata
 rem kkshibat@calpoly.edu
 
 create table list (
-   LastName varchar2(64),
-   FirstName varchar2(64),
+   LastName varchar2(30),
+   FirstName varchar2(30),
    Grade integer,
    classroom integer,
    PRIMARY KEY(LastName, FirstName, Grade, classroom)
 );
 
 create table teachers (
-   LastName varchar(64),
-   FirstName varchar(64),
+   LastName varchar(30),
+   FirstName varchar(30),
    Classroom integer PRIMARY KEY
 );
 
